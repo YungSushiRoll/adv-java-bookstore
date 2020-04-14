e9ba0aad327cd9867ac367eaf27431e881e18b94
YungSushiRoll <j.gosset1@gmail.com>
2020-04-13 19:56:19 -0500

    testing

--- src/isp/lab1/Main.java
+++ src/isp/lab1/Main.java
@@ -21,5 +21,6 @@ public class Main {
         System.out.println(book2.toString());
         System.out.println("\n");
         System.out.println(record.toString());
+        // test commit
     }
 }
